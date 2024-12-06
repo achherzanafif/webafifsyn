a9dc6debe3b578554fac8f5426e67a5f24d858b3
achherzanafif <afifunited93@gmail.com>
2024-12-06 21:20:27 +0700

    add main

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('welcome');});
-//Route::get('/contact', function () { return view('contact');});
 
-//Route::get('/login', function () { return view('login');});
+Route::get('/login', function () { return view('login');});
 
 //Route::get('/webafif', function () { return view('webafif');});
 
-//Route::get('/tentang', function () { return view('tentang');});
+Route::get('/index', function () { return view('index');});
 
-//Route::view('/profile', 'profile');
 
-Route::get('/login' ,[AuthController::class, 'login']);
+Route::get('/login', [AuthController::class, 'index']);
